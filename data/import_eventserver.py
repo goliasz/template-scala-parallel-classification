f8ccd890417740b2191bbc2c0d75b1fd5428ac45
goliasz <piotr.goliasz@post.pl>
2015-12-13 16:26:00 +0100

    25 attr

--- data/import_eventserver.py
+++ data/import_eventserver.py
@@ -18,10 +18,32 @@ def import_events(client, file):
       entity_type="user",
       entity_id=str(count), # use the count num as user ID
       properties= {
-        "attr0" : int(attr[0]),
-        "attr1" : int(attr[1]),
-        "attr2" : int(attr[2]),
-        "plan" : int(plan)
+        "attr0" : double(attr[0]),
+        "attr1" : double(attr[1]),
+        "attr2" : double(attr[2]),
+        "attr3" : double(attr[3]),
+        "attr4" : double(attr[4]),
+        "attr5" : double(attr[5]),
+        "attr6" : double(attr[6]),
+        "attr7" : double(attr[7]),
+        "attr8" : double(attr[8]),
+        "attr9" : double(attr[9]),
+        "attr10" : double(attr[10]),
+        "attr11" : double(attr[11]),
+        "attr12" : double(attr[12]),
+        "attr13" : double(attr[13]),
+        "attr14" : double(attr[14]),
+        "attr15" : double(attr[15]),
+        "attr16" : double(attr[16]),
+        "attr17" : double(attr[17]),
+        "attr18" : double(attr[18]),
+        "attr19" : double(attr[19]),
+        "attr20" : double(attr[20]),
+        "attr21" : double(attr[21]),
+        "attr22" : double(attr[22]),
+        "attr23" : double(attr[23]),
+        "attr24" : double(attr[24]),
+        "plan" : double(plan)
       }
     )
     count += 1
