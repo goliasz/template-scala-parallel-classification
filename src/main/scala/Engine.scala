2278e56b019fbef28023b5c42f3d61500ff39ac6
goliasz <piotr.goliasz@post.pl>
2015-12-13 16:07:13 +0100

    25 attr

--- src/main/scala/Engine.scala
+++ src/main/scala/Engine.scala
@@ -6,7 +6,29 @@ import io.prediction.controller.Engine
 class Query(
   val attr0 : Double,
   val attr1 : Double,
-  val attr2 : Double
+  val attr2 : Double,
+  val attr3 : Double,
+  val attr4 : Double,
+  val attr5 : Double,
+  val attr6 : Double,
+  val attr7 : Double,
+  val attr8 : Double,
+  val attr9 : Double,
+  val attr10 : Double,
+  val attr11 : Double,
+  val attr12 : Double,
+  val attr13 : Double,
+  val attr14 : Double,
+  val attr15 : Double,
+  val attr16 : Double,
+  val attr17 : Double,
+  val attr18 : Double,
+  val attr19 : Double,
+  val attr20 : Double,
+  val attr21 : Double,
+  val attr22 : Double,
+  val attr23 : Double,
+  val attr24 : Double
 ) extends Serializable
 
 class PredictedResult(
